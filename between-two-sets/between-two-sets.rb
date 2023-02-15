b923ecf599adfe44647b49b737289acf96906cff
Caio Matheus <86628124+caieras@users.noreply.github.com>
2023-02-15 18:18:09 +0000

    between two sets

---
+++ between-two-sets/between-two-sets.rb
@@ -0,0 +1,46 @@
+#!/bin/ruby
+
+require 'json'
+require 'stringio'
+
+#
+# Complete the 'getTotalX' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY a
+#  2. INTEGER_ARRAY b
+#
+
+def getTotalX(a, b)
+  result = 0
+  multiple = 0
+  lcm = a.reduce(:lcm)
+  gcd = b.reduce(:gcd)
+  
+  while multiple <= gcd do
+    multiple += lcm
+    result += 1 if gcd % multiple == 0
+  end
+  
+  result
+end
+
+fptr = File.open(ENV['OUTPUT_PATH'], 'w')
+
+first_multiple_input = gets.rstrip.split
+
+n = first_multiple_input[0].to_i
+
+m = first_multiple_input[1].to_i
+
+arr = gets.rstrip.split.map(&:to_i)
+
+brr = gets.rstrip.split.map(&:to_i)
+
+total = getTotalX arr, brr
+
+fptr.write total
+fptr.write "\n"
+
+fptr.close()
